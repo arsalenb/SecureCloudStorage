3ef412130639db1217e7085277734868efc0d655
bashar-22 <bh9466@srmist.edu.in>
2023-12-02 14:44:40 -0800

    send and verify server certificate

---
+++ Client/client.cpp
@@ -0,0 +1,227 @@
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <openssl/bio.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+using namespace std;
+const int PORT = 8080;
+const int MAX_CERTIFICATE_SIZE = 4096;
+const int MAX_USERNAME_LENGTH = 5;
+
+bool verifyServerCertificate(X509 * caCert, X509_CRL * crl, X509 *serverCert) {
+   
+    // Create a store and add the CA certificate and CRL to it
+    X509_STORE *store = X509_STORE_new();
+    X509_STORE_add_cert(store, caCert);
+    X509_STORE_add_crl(store, crl);
+
+    // Set the flags to check against the CRL
+    X509_STORE_set_flags(store, X509_V_FLAG_CRL_CHECK | X509_V_FLAG_CRL_CHECK_ALL);
+
+    // Create a context and set the store
+    X509_STORE_CTX *ctx = X509_STORE_CTX_new();
+    X509_STORE_CTX_init(ctx, store, serverCert, nullptr);
+
+    // Perform the verification
+    int ret = X509_verify_cert(ctx);
+    if (ret != 1) {
+        fprintf(stderr, "Certificate verification failed\n");
+        ERR_print_errors_fp(stderr);
+    } else {
+        printf("Certificate verification succeeded\n");
+    }
+
+    // Clean up
+    X509_STORE_CTX_free(ctx);
+    X509_STORE_free(store);
+    X509_free(serverCert);
+    X509_free(caCert);
+    X509_CRL_free(crl);
+
+    return ret == 1;
+}
+// Function to receive server certificate from the server
+bool receiveServerCertificate(int clientSocket, X509*& serverCert) {
+    // Receive the certificate size
+    int certSize;
+    ssize_t bytesReceived = recv(clientSocket, &certSize, sizeof(certSize), 0);
+    if (bytesReceived <= 0) {
+        std::cerr << "Error receiving certificate size" << std::endl;
+        return false;
+    }
+    if(certSize>MAX_CERTIFICATE_SIZE)
+    {
+        std::cerr << "Certificate size exceeds the max size" << std::endl;
+        return false;
+    }
+
+    // Receive the certificate data
+    char certBuffer[certSize]={0};
+    bytesReceived = recv(clientSocket, certBuffer, certSize, 0);
+    if (bytesReceived <= 0) {
+        std::cerr << "Error receiving certificate data" << std::endl;
+        return false;
+    }
+
+    // Create a BIO from the received data
+    BIO* bio = BIO_new_mem_buf(certBuffer, certSize);
+    if (!bio) {
+        std::cerr << "Error creating BIO from certificate data" << std::endl;
+        return false;
+    }
+
+    // Read the X509 certificate from the BIO
+   
+   serverCert = PEM_read_bio_X509(bio, NULL, NULL, NULL);
+  if (!serverCert) {
+    std::cerr << "Error reading X509 certificate from BIO" << std::endl;
+    
+    ERR_print_errors_fp(stderr); // Print OpenSSL error information
+    BIO_free(bio);
+   
+    return false;
+}
+    
+// Print serial number
+ASN1_INTEGER* serialNumber = X509_get_serialNumber(serverCert);
+BIGNUM* bnSerial = ASN1_INTEGER_to_BN(serialNumber, nullptr);
+char* serialHex = BN_bn2hex(bnSerial);
+
+cout << "Serial Number: " << serialHex<< endl;
+OPENSSL_free(serialHex);
+BN_free(bnSerial);
+    // Clean up
+    BIO_free(bio);
+
+    return true;
+}
+
+
+int main() {
+    
+    // Create socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "Error creating client socket" << std::endl;
+        return -1;
+    }
+
+    // Server address
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/Address not supported" << std::endl;
+        close(clientSocket);
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error connecting to server" << std::endl;
+        close(clientSocket);
+        return -1;
+    }
+
+    // Get username from user
+    std::string userName;
+    std::cout << "Enter your username (up to 5 characters): ";
+    std::cin >> userName;
+
+    // Enforce the maximum username length
+    if (userName.size() > MAX_USERNAME_LENGTH) {
+        std::cerr << "Error: Username is too long. Maximum length is 5 characters." << std::endl;
+        // Handle the error, return, or take appropriate action
+        return 1;
+    }
+
+    // Send username to server
+    send(clientSocket, userName.c_str(), userName.size(), 0);
+
+
+
+    // Receive result from server
+    int responseSize=1;
+    char buffer[responseSize+1] = {0}; //+1 for null terminator
+    ssize_t bytesRead = recv(clientSocket, buffer, responseSize, 0);
+    if (bytesRead <= 0) {
+        std::cerr << "Error receiving result from server" << std::endl;
+    } else {
+        std::string serverResponse(buffer);
+      std::cout << "Server response: " << buffer << std::endl;
+
+    // Check the server response to determine the next steps
+    if (serverResponse.compare("1") == 0) {
+        // User exists, proceed to receive the server certificate
+        X509* serverCert = nullptr;
+        if (receiveServerCertificate(clientSocket, serverCert)) {
+            // Use the server certificate as needed
+            std::cout << "Received server certificate successfully" << std::endl;
+
+            const char *caCertFile = "Cloud Storage CA_cert.pem";
+            const char *crlFile = "Cloud Storage CA_crl.pem";
+            // Load CA certificate
+        X509 *caCert = nullptr;
+        FILE *caCertFilePtr = fopen(caCertFile, "r");
+        if (!caCertFilePtr) {
+            perror("Error opening CA certificate file");
+            return 0;
+        }
+
+        caCert = PEM_read_X509(caCertFilePtr, nullptr, nullptr, nullptr);
+        fclose(caCertFilePtr);
+
+        if (!caCert) {
+            ERR_print_errors_fp(stderr);
+            return 0;
+        }
+
+        // Load CRL
+        X509_CRL *crl = nullptr;
+        FILE *crlFilePtr = fopen(crlFile, "r");
+        if (!crlFilePtr) {
+            perror("Error opening CRL file");
+            X509_free(caCert);
+            return 0;
+        }
+
+        crl = PEM_read_X509_CRL(crlFilePtr, nullptr, nullptr, nullptr);
+        fclose(crlFilePtr);
+
+        if (!crl) {
+            ERR_print_errors_fp(stderr);
+            X509_free(caCert);
+            return 0;
+        }
+        bool verified=verifyServerCertificate(caCert, crl,serverCert);
+        if (verified) {
+        std::cout << "Server certificate verification successful." << std::endl;
+
+        } else {
+            std::cerr << "Server certificate verification failed." << std::endl;
+        }
+            // Clean up
+            X509_free(serverCert);
+        } else {
+            std::cerr << "Error receiving server certificate" << std::endl;
+        }
+    } else if (strcmp(buffer, "0") == 0) {
+        // User does not exist, handle accordingly (close the connection or take other actions)
+        std::cerr << "User does not exist" << std::endl;
+        // You might want to close the client socket or take other appropriate actions here
+    } else {
+        // Unexpected response from the server, handle accordingly
+        std::cerr << "Unexpected server response" << std::endl;
+    }
+
+
+    }
+
+    // Close client socket
+    close(clientSocket);
+
+    return 0;
+}
---
+++ Server/server.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <vector>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+using namespace std;
+const int PORT = 8080;
+const int BUFFER_SIZE = 4096;
+const int MAX_USERNAME_LENGTH = 5;
+
+
+
+// Function to handle each connected client
+void handleClient(int clientSocket, const std::vector<std::string>& userNames) {
+    char buffer[MAX_USERNAME_LENGTH+1] = {0}; // +1 for null terminator  
+    ssize_t bytesRead = recv(clientSocket, buffer, MAX_USERNAME_LENGTH, 0);
+
+    if (bytesRead <= 0) {
+        std::cerr << "Error receiving username from client" << std::endl;
+        close(clientSocket);
+        return;
+    }
+
+    std::string receivedUserName(buffer);
+    std::cout << "Received username from client: " << receivedUserName << std::endl;
+
+    // Check if username exists
+    bool usernameExists = false;
+    for (const auto& user : userNames) {
+        if (user == receivedUserName) {
+            usernameExists = true;
+            break;
+        }
+    }
+    // Send result back to client
+    std::string resultMsg = (usernameExists) ? "1" : "0";
+    send(clientSocket, resultMsg.c_str(), resultMsg.size(), 0);
+
+    if (usernameExists) {
+        // Load the server certificate from PEM file
+         FILE* server_certificate_file = fopen("Cloud Strorage Server_cert.pem", "r");
+        if (!server_certificate_file) {
+            std::cerr << "Error loading server certificate" << std::endl;
+             std::cout.flush();
+            close(clientSocket);
+            return;
+        }
+
+        X509* serverCert = PEM_read_X509(server_certificate_file, NULL, NULL, NULL);
+        fclose(server_certificate_file);
+
+        if (!serverCert) {
+            std::cerr << "Error reading server certificate" << std::endl;
+             std::cout.flush();
+            close(clientSocket);
+            return;
+        }
+
+        // Send server certificate to client
+        BIO* bio = BIO_new(BIO_s_mem());
+        if (!bio) {
+            std::cerr << "Error creating BIO" << std::endl;
+             std::cout.flush();
+            X509_free(serverCert);
+            close(clientSocket);
+            return;
+        }
+         
+
+        int result=PEM_write_bio_X509(bio, serverCert);
+
+        if (!result) {
+        std::cerr << "[-] (CertificateStore) Failed to write the certificate in the BIO" << endl; 
+          std::cerr.flush();
+           std::cout.flush();
+        BIO_free(bio);
+        return ;
+    }
+
+    // trucate to the size of the certificate
+        char certBuffer[BUFFER_SIZE];
+        int certSize = BIO_read(bio, certBuffer, sizeof(certBuffer));
+        BIO_free(bio);
+
+       std::cout << "size of certificate " +certSize;
+        std::cout.flush();
+        // Send the certificate size to the client
+        send(clientSocket, &certSize, sizeof(certSize), 0);
+
+        // Send the certificate data to the client
+        send(clientSocket, certBuffer, certSize, 0);
+
+        // Clean up
+        X509_free(serverCert);
+    } else {
+        // If username does not exist
+      
+       
+    }
+
+    // Close client socket
+    close(clientSocket);
+}
+
+
+int main() {
+    // Create socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Error creating server socket" << std::endl;
+        return -1;
+    }
+
+    // Bind socket to port
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error binding socket to port" << std::endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    // Listen for connections
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "Error listening for connections" << std::endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    std::cout << "Server listening on port " << PORT << "..." << std::endl;
+
+    // List of usernames
+    std::vector<std::string> userNames = {"user1", "user2", "user3"};
+
+    while (true) {
+        // Accept connection
+        sockaddr_in clientAddr{};
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        if (clientSocket == -1) {
+            std::cerr << "Error accepting connection" << std::endl;
+            continue;
+        }
+
+        // Create a new thread for each connected client
+        std::thread t1(&handleClient, clientSocket, std::cref(userNames));
+       t1.detach();
+
+        
+    }
+
+    // Close server socket (This part will never be reached in this example)
+    close(serverSocket);
+
+    return 0;
+}
+
+
